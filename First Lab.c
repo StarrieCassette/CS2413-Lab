776fbd97286e0da53912ce750594133550b9968a
Manny <matamorosemmanuel4@gmail.com>
2025-02-27 15:06:15 -0600

    Update First Lab.c

--- First Lab.c
+++ First Lab.c
@@ -1,5 +1,3 @@
-//Emmanuel Matamoros R11854157
-
 //203. Remove Linked List Elements
 struct ListNode* removeElements(struct ListNode* head, int val) {
         while (head != NULL && head->val == val){
